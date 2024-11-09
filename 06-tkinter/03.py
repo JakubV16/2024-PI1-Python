5ecf66e7147af815153e6606a0b451d91913612e
Jakub Vrškový <jakub.vrskovy08@gmail.com>
2024-11-09 12:07:32 +0100

    Add 03.py DU

--- 06-tkinter/03.py
+++ 06-tkinter/03.py
@@ -2,7 +2,36 @@ import tkinter
 
 canvas = tkinter.Canvas(width=320, height=320)
 canvas.pack()
-canvas.create_rectangle(0, 0, 10, 10,)
+canvas.create_rectangle(30, 10, 40, 20, fill = "black")
+canvas.create_rectangle(40, 10, 50, 20, fill = "black")
+canvas.create_rectangle(50, 10, 60, 20, fill = "black")
+canvas.create_rectangle(40, 20, 50, 30, fill = "black")
+canvas.create_rectangle(40, 30, 50, 40, fill = "black")
+canvas.create_rectangle(40, 40, 50, 50, fill = "black")
+canvas.create_rectangle(40, 50, 50, 60, fill = "black")
+canvas.create_rectangle(40, 60, 50, 70, fill = "black")
+canvas.create_rectangle(10, 60, 20, 70, fill = "black")
+canvas.create_rectangle(20, 70, 30, 80, fill = "black")
+canvas.create_rectangle(30, 70, 40, 80, fill = "black")
+
+canvas.create_rectangle(80, 10, 90, 20, fill = "black")
+canvas.create_rectangle(80, 20, 90, 30, fill = "black")
+canvas.create_rectangle(80, 30, 90, 40, fill = "black")
+canvas.create_rectangle(80, 40, 90, 50, fill = "black")
+canvas.create_rectangle(90, 50, 100, 60, fill = "black")
+canvas.create_rectangle(90, 60, 100, 70, fill = "black")
+canvas.create_rectangle(100, 70, 110, 80, fill = "black")
+canvas.create_rectangle(120, 10, 130, 20, fill = "black")
+canvas.create_rectangle(120, 20, 130, 30, fill = "black")
+canvas.create_rectangle(120, 30, 130, 40, fill = "black")
+canvas.create_rectangle(120, 40, 130, 50, fill = "black")
+canvas.create_rectangle(110, 50, 120, 60, fill = "black")
+canvas.create_rectangle(110, 60, 120, 70, fill = "black")
+
+canvas.create_rectangle(60, 70, 70, 80, fill = "black")
+canvas.create_rectangle(130, 70, 140, 80, fill= "black")
+
+
 
 
 tkinter.mainloop()
