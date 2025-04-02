ee319ae7059b122bf85941950b9b07a19fa2e7f2
Jakub Vrškový <jakub.vrskovy08@gmail.com>
2025-04-02 10:03:30 +0200

    add 16 list

--- 15-string 2.0/04-du.py
+++ 15-string 2.0/04-du.py
@@ -1,2 +1,24 @@
 # uzivatel zada celu vetu a program vetu rozdeli na slova , ktore vypise nahodnou farbou na nahodnom mieste okej !!!
 # aj písmenka zo slov vypise nahodnymi farbami
+
+
+import tkinter
+import random
+canvas = tkinter.Canvas(width=800 , height=800)
+canvas.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+tkinter.mainloop()
