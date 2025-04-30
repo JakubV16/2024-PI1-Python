798f72750c361774c2e3b53e9000754f2fff6d3c
Jakub Vrškový <jakub.vrskovy08@gmail.com>
2025-04-30 09:14:52 +0200

    add 19 subory

---
+++ 19-textove subory2/01.py
@@ -0,0 +1,34 @@
+import tkinter
+import random
+
+canvas = tkinter.Canvas(width=800, height=800)
+canvas.pack()
+
+riadok=[]
+# ftvary=open('tvary.txt', 'w' )
+tvary=random.choice('ORL')
+riadok.append(tvary)
+print(tvary)
+
+for i in range(4):
+    suradnice=random.randint(3,797)
+    riadok.append(suradnice)
+    print(suradnice)
+
+for i in range(3):
+    farba=random.randint(0,255)
+    riadok.append(farba)
+    print(farba)
+
+print(riadok)
+
+
+
+
+
+
+
+
+
+
+tkinter.mainloop()
